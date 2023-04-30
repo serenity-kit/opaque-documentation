f88bc175b38519a1f464f936cf31d3f0e6097abb
Nik Graf <nik@nikgraf.com>
2023-04-30 22:40:00 +0200

    setup documentation

---
+++ README.md
@@ -0,0 +1,19 @@
+# Opaque Documentation
+
+## Local Development
+
+```sh
+pnpm i
+```
+
+Then, run
+
+```sh
+pnpm dev
+```
+
+to start the development server and visit localhost:3000.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Opaque</span>,
+  project: {
+    link: "https://github.com/serenity-kit/opaque",
+  },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  docsRepositoryBase: "https://github.com/serenity-kit/opaque-documentation",
+  footer: {
+    text: "Opaque",
+  },
+};
+
+export default config;
