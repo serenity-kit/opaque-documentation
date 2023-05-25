3c805a7913040a1fe62900afa4e334ac483403b0
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-05-25 11:33:02 +0200

    Add serverSetup for InteractiveForm

--- components/InteractiveForm.tsx
+++ components/InteractiveForm.tsx
@@ -1,3 +1,14 @@
+import * as opaque from "@serenity-kit/opaque";
+
+// represents the secret of the server
+// which is a combination of
+// - a private key
+// - an oprf-seed to initialize the oprf function
+// - a fake-private key for failed login attempts
+const serverSetup = opaque.serverSetup();
+
 export const InteractiveForm = () => {
+  console.log(serverSetup);
+
   return <div>blub</div>;
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+const nextConfig = {
+  webpack(config, { isServer, dev }) {
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? '../static/wasm/[modulehash].wasm'
+        : 'static/wasm/[modulehash].wasm'
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+
+    return config
+  },
+}
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra(nextConfig)
