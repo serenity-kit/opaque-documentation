131b582267cd722e41fe20c75f17648384c69f0d
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-06-16 15:36:23 +0200

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+// needed as explicit import before our own styles
+// as they are otherwise added last and would therefore overrule our stylesheet
 import "nextra-theme-docs/style.css";
 import "../styles/global.css";
 
