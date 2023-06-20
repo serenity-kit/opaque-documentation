c139be75cbfb0d3f7b3c70bb91c8c9df8a2e8bfe
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-06-20 19:38:51 +0200

    Add and use Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 // needed as explicit import before our own styles
 // as they are otherwise added last and would therefore overrule our stylesheet
 import "nextra-theme-docs/style.css";
+import { Inter } from "next/font/google";
 import "../styles/global.css";
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
