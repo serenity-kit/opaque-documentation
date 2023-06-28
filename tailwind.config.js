5153b9c57e2555031a2bc3f9ba4265632745934b
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-06-28 09:21:47 +0200

    Enable dark-mode class toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     colors: {
       transparent: "transparent",
