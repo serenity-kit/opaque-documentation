fa328502b23f9ad466f40344bf04ae720bfd8266
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-09-06 16:33:28 +0200

    Fix Blockwrapper classes

--- components/blocks/BlockWrapper.tsx
+++ components/blocks/BlockWrapper.tsx
@@ -1,14 +1,22 @@
 import { ReactElement } from "react";
+import cn from "clsx";
 
 export type BlockWrapperProps = ReactElement & {};
 
-export const BlockWrapper = ({ children, ...props }): BlockWrapperProps => {
+export const BlockWrapper = ({
+  className,
+  children,
+  ...props
+}: BlockWrapperProps) => {
   return (
     <div
-      className="h-[50rem] max-w-[67.5rem] mx-auto bg-gray-300 flex items-center"
+      className={cn(
+        "h-[50rem] max-w-[67.5rem] mx-auto bg-gray-150 flex items-center",
+        className
+      )}
       {...props}
     >
-      <div>{children}</div>
+      <div className="w-full">{children}</div>
     </div>
   );
 };
