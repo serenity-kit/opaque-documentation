89e6001711766b14da70201f3ccff7b794f3cd75
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-09-11 14:59:12 +0200

    Move th styling into component

--- components/Th.tsx
+++ components/Th.tsx
@@ -6,6 +6,7 @@ export const Th = ({ className = "", ...props }: ComponentProps<"th">) => (
     className={cn(
       "nx-m-0 py-3 px-4 nx-font-semibold text-left bg-gray-150 border-surface-border",
       "dark:bg-dark-surface-tertiary dark:border-dark-surface-border",
+      "first:rounded-l-lg last:rounded-r-lg",
       className
     )}
     {...props}
