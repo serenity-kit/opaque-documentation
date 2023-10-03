413172eb9d3f09eeea1a2628abae380438cf17c5
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-10-03 17:17:57 +0200

    Add opaque-icon to cli-prompt, inprove styling, and add comment option

--- components/CliTypeWriter.tsx
+++ components/CliTypeWriter.tsx
@@ -1,29 +1,45 @@
 import { ComponentProps } from "react";
 import cn from "clsx";
 import { TypeAnimation } from "react-type-animation";
+import { Icon } from "./icon/Icon";
 
 export type CliTypeWriterProps = ComponentProps<"div"> & {
   sequence?: (string | number)[];
   slow?: boolean;
   prompt?: boolean;
+  comment?: boolean;
 };
 
 export const CliTypeWriter = ({
   sequence,
   slow,
   prompt,
+  comment,
   className,
   ...props
 }: CliTypeWriterProps) => {
   const styles = {
-    wrapper: "flex gap-3",
+    prompt: "flex gap-3 font-bold tracking-wider",
+    comment: "italic",
   };
 
   const typeSpeed = prompt || slow ? 40 : 80;
 
   return (
-    <div className={cn(prompt && styles.wrapper, className)} {...props}>
-      {prompt ? <div>&rarr;&nbsp;&sim;</div> : null}
+    <div
+      className={cn(
+        prompt && styles.prompt,
+        comment && styles.comment,
+        className
+      )}
+      {...props}
+    >
+      {prompt ? (
+        <div className="flex">
+          <Icon name="opaque-key" color="white" />
+          &nbsp;<span className={"text-color-actor"}>&sim;</span>
+        </div>
+      ) : null}
       <div>
         <TypeAnimation sequence={sequence} speed={typeSpeed} cursor={false} />
       </div>
